196349c26f2b47a5581b8192109d8b54d2cfdd37
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2020-12-21 21:43:17 +0700

    Add project files.

---
+++ ClientRest_015_MuhammadDzalfiqri/ClassData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace ClientRest_015_MuhammadDzalfiqri
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+
+            foreach(var mhs in data)
+            {
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
